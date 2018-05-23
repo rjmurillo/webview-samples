46212a45178d4c904888c763ee68e38dd16863dd
Richard Murillo <rimuri@microsoft.com>
2018-05-23 15:39:44 -0700

    Add DesktopBridge projects

---
+++ Microsoft.Toolkit.Win32.Samples.WPF.WebView/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("Microsoft.Toolkit.Win32.Samples.WPF.WebView.exe")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
